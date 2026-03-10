a206d3a87ba5360bcd5fa3a1b588518f6f45e83f
priyansh796 <priyanshvats@gmail.com>
2026-03-10 15:47:53 +0530

    Update stock_bot_monthly.py

--- stock_bot_monthly.py
+++ stock_bot_monthly.py
@@ -16,8 +16,8 @@ PORTFOLIO_FILE = "portfolio.xlsx"
 
 SPREADSHEET_NAME = "Stock Bot Dashboard"
 
-TELEGRAM_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
-CHAT_ID = "YOUR_CHAT_ID"
+TELEGRAM_TOKEN = 8630503074:AAHgONEVwJB_QVZ1GeKBaVGl9Z3Ct0E_yLw
+CHAT_ID = 8258280498
 
 
 def super_smoother(price, period):
@@ -318,5 +318,6 @@ send_telegram_message(message)
 
 print("Telegram notification sent.")
 ```
+
 
 
